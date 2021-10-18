2eb42b65e67f5915c398bc733eec7b5586d95c2a
perkynades <emil.nilsen9888@gmail.com>
2021-10-18 12:57:25 +0200

    Add working sudoku solver

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement, useState } from 'react'
 import './App.css'
 import SudokuGrid from './SudokuGrid'
+import { sudokuSolver } from './sudokuSolver'
 
 function App(): ReactElement {
-    const [sudokuGrid, setSudokuGrid] = useState<number[][]>([])
+    let sudokuGrid: number[][] = []
+    const [grid, setGrid] = useState<number[][]>([])
 
     const [parsed, setParsed] = useState<string | ArrayBuffer | undefined | null>()
 
@@ -29,71 +31,13 @@ function App(): ReactElement {
         while (array.length) {
             newArray.push(array.splice(0, 9))
         }
-        setSudokuGrid(newArray)
+        sudokuGrid = newArray
     }
 
-    const possible = (y: number, x: number, n: number): boolean => {
-        let isPossible: boolean = true
-
-        for (let i = 0; i < 9; i++) {
-            if (sudokuGrid[y][i] === n) {
-                isPossible = false
-            }
-        }
-        for (let i = 0; i < 9; i++) {
-            if (sudokuGrid[i][x] === n) {
-                isPossible = false
-            }
-        }
-
-        let x0 = (x / 3) * 3
-        let y0 = (y / 3) * 3
-
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (sudokuGrid[y0 + i][x0 + j] === n) {
-                    isPossible = false
-                }
-            }
-        }
-
-        return isPossible
-    }
-
-    const solveSudoku = () => {
-        for (let y = 0; y <= 8; y++) {
-            for (let x = 0; x <= 8; x++) {
-                if (sudokuGrid[y][x] === 0) {
-                    for (let n = 1; n <= 9; n++) {
-                        if (possible(y, x, n)) {
-                            const copy = [...sudokuGrid]
-                            copy[y][x] = n
-                            setSudokuGrid(copy)
-
-                            solveSudoku()
-
-                            const newCopy = [...sudokuGrid]
-                            newCopy[y][n] = 0
-                            setSudokuGrid(newCopy)
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    const exists = (value: number): boolean => {
-        let isExists = false
-
-        sudokuGrid.forEach((row) => {
-            for (let i = 0; i < row.length; i++) {
-                if (row[i] === value) {
-                    isExists = true
-                }
-            }
-        })
-
-        return isExists
+    const solve = () => {
+        sudokuSolver(sudokuGrid)
+        console.log(sudokuGrid)
+        setGrid(sudokuGrid)
     }
 
     return (
@@ -104,8 +48,8 @@ function App(): ReactElement {
             </label>
 
             <button onClick={parseFileString}>Create sudoku grid</button>
-            <SudokuGrid sudokuGrid={sudokuGrid} />
-            <button onClick={solveSudoku}>Start solving</button>
+            <SudokuGrid sudokuGrid={grid} />
+            <button onClick={solve}>Start solving</button>
         </div>
     )
 }
